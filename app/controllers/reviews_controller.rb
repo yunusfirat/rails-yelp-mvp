6f6fef34a06bc7e65ed80b5991dcd0e1ab7f6f3f
yunusfirat <yunusfirat44@gmail.com>
2021-02-17 17:07:01 +0000

    Some other method is improved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+ before_action :set_restaurant, only: [ :show, :edit ]
+  def new
+    @reviews = Review.new
+  end
+
+  def create
+    review = Review.new(review_params)
+    review.save
+    redirect_to reviews_path
+  end
+
+  private
+    
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
